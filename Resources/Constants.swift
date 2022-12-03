d166f788fe037c110d249dee56cd91e8cbf71786
Vishruti <vishrutigopani1996@gmail.com>
2022-12-03 15:09:30 -0500

    Initial commit, Added assets and constants setup

---
+++ Resources/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Guidomia
+//
+//  Created by Vish on 2022-12-03.
+//
+
+import Foundation
+
+struct Constants {
+    static let star: String = "★"
+    static let bulletPoint: String = "•"
+    static let price: String = "Price:"
+    static let k: String = "k"
+    static let newLine: String = "\n"
+}
+
